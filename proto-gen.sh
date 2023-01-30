4bfe98ed5de98b6c6f9408002904ee3ec1563d38
Tonipenyallop <kimtesu521@gmail.com>
2023-01-30 20:13:18 +0900

    update: bash file

--- proto-gen.sh
+++ proto-gen.sh
@@ -1,1 +1,8 @@
 ./node_modules/.bin/proto-loader-gen-types --grpcLib=@grpc/grpc-js --outDir=proto proto/*.proto
+
+mkdir -p ./client_grpc_tutorial/src/proto
+
+protoc -I=. ./proto/*.proto \
+  --js_out=import_style=commonjs:./client_grpc_tutorial/src \
+  --grpc-web_out=import_style=typescript,mode=grpcwebtext:./client_grpc_tutorial/src
+# 
